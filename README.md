7cad72da1a0320697cd535133be94f7a55337fba
Mish Ushakov <mishushakov@users.noreply.github.com>
2021-08-09 16:05:01 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,138 @@
+# Floating Chat web component
+
+![Floating Chat on desktop](./screen-desktop.png)
+
+![Floating Chat on mobile](./screen-mobile.png)
+
+[Demo](https://mishushakov.github.io/floating-chat)
+
+A framework-independent floating chat window web component written in [Svelte](https://github.com/sveltejs/svelte)
+
+The primary purpose of it is embedding engageable content, in particular: helpdesks, chatbots or anything else that requires a floating box
+
+Features:
+
+- Cross-framework integration (esmodule & umd)
+- Responsive UI with animations
+- Accessibility features
+- Customisation options as well
+- Custom javascript events
+
+## Installation
+
+### Browser
+
+Add the following script to your `<head>`
+
+```html
+<script defer src='https://unpkg.com/floating-chat@latest/dist/index.js'></script>
+```
+
+### Using NPM
+
+Install the `floating-chat` package
+
+```sh
+npm i floating-chat
+```
+
+Then import into your application with
+
+```js
+import floatingChat from 'floating-chat'
+```
+
+Add the tag to your template
+
+```html
+<floating-chat content="https://example.com" />
+```
+
+You can see the demo example in [public/index.html](public/index.html)
+
+## Customisation
+
+![Content area](./screen-area.png)
+
+Content area
+
+### HTML properties
+
+Available attributes:
+
+- `content`, content address (to be displayed in `<iframe>`)
+- `height`, floating window height, default `600px`, always `100%` on mobile
+- `width`, floating window width, default `400px`, always `100%` on mobile
+- `textOpen`, text when the window is closed, default `Open`, hidden on mobile
+- `textClose`, text when the window is opened, default `Close`, hidden on mobile
+- `textColor` - the colour of text, default `#000000` (black)
+- `background` - background color, can be either a color code or a css `background` property, default `#FFFFFF` (white)
+- `logo` - logo url, css `background` property, eg `url(https://storage.googleapis.com/cloudprod-apiai/a0b2e356-b43e-4ca5-a094-b219475fa4ca_x.png)`
+- `position` - y, x position on screen, default `bottom right`, possible options `bottom`, `top`, `left`, `right`
+- `font` - font name
+- `borderRadius` - corner radius, default `28px`, `0px` on mobile when opened
+- `opened` - boolean, whether floating window should start opened, default `false`
+
+### Styling with CSS variables
+
+Available variables:
+
+- `--height`, floating window height, default `600px`, always `100%` on mobile
+- `--width`, floating window width, default `400px`, always `100%` on mobile
+- `--textcolor` - the colour of text, default `#000000` (black)
+- `--background` - background color, can be either a color code or a css `background` property, default `#FFFFFF` (white)
+- `--logo` - logo url, css `background` property, eg `url(https://storage.googleapis.com/cloudprod-apiai/a0b2e356-b43e-4ca5-a094-b219475fa4ca_x.png)`
+- `--positiony` - y position on screen, default `bottom`
+- `--positionx` - x position on screen, default `right`
+- `--font` - font name
+- `--borderradius` - corner radius, default `28px`, `0px` on mobile when opened
+
+## Events
+
+Following events are emmited:
+
+- `open`, when the floating window state went from closed to open
+- `close`, when the floating window state went from open to closed
+- `toggle`, when the floating window state was toggled
+
+Example in Vue:
+
+```html
+<template>
+  <floating-chat @open="open" />
+</template>
+
+<script>
+export default {
+  methods: {
+    open () {
+      alert('Opened!')
+    }
+  }
+}
+</script>
+```
+
+## Development
+
+1. Clone the repository
+
+    ```
+    git clone
+    ```
+
+2. Install the dependencies
+
+    ```
+    npm i
+    ```
+
+3. Build
+
+    ```
+    npm run build
+    ```
+
+## Acknowledgements
+
+- [Icons by Feather](https://github.com/feathericons/feather)
---
+++ src/FloatingChat.svelte
@@ -0,0 +1,152 @@
+<svelte:options tag="floating-chat" />
+
+<script>
+  import { get_current_component } from 'svelte/internal'
+  const component = get_current_component()
+
+	export let content
+	export let height = '600px'
+	export let width = '400px'
+	export let textopen = 'Open'
+	export let textclose = 'Close'
+	export let textcolor = '#000000'
+	export let background = '#FFFFFF'
+	export let logo
+	export let position = 'bottom right'
+	export let font
+	export let borderradius = '28px'
+	export let opened = false
+
+	const positiony = position.split(' ')[0] === 'top' ? 'initial': '0'
+	const positionx = position.split(' ')[1] === 'left' ? 'initial': '0'
+
+	export function open () {
+		opened = true
+    component?.dispatchEvent(new CustomEvent('open', { detail: null, composed: true }))
+	}
+
+	export function close () {
+		opened = false
+		component?.dispatchEvent(new CustomEvent('close', { detail: null, composed: true }))
+	}
+
+	export function toggle () {
+		opened ? close() : open()
+    component?.dispatchEvent(new CustomEvent('toggle', { detail: null, composed: true }))
+	}
+</script>
+
+<main>
+	<button
+    title="{opened ? textclose: textopen}"
+    aria-label="{opened ? textclose: textopen}"
+		class="df-btn {opened ? 'df-opened': 'df-closed'}"
+		style="--height: {height}; --width: {width}; --textcolor: {textcolor}; --background: {background}; --borderradius: {borderradius}; --font: {font}; --logo: {logo}; --positiony: {positiony}; --positionx: {positionx}"
+		on:click={toggle}
+	>
+		<div class="df-btn-text">{opened ? textclose : textopen}</div>
+		<div class="df-btn-content">
+			<iframe src="{content}" title="{textopen}" allow="microphone *"></iframe>
+		</div>
+	</button>
+</main>
+
+<style>
+	.df-btn {
+    padding: 0;
+    border: none;
+    box-shadow: 0 1px 2px 0 rgba(60,64,67,0.302),0 1px 3px 1px rgba(60,64,67,0.149);
+    font-family: var(--font), -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: var(--background);
+    border-radius: var(--borderradius);
+    cursor: pointer;
+    transition: all .45s cubic-bezier(.4, 0, .2, 1);
+    position: fixed;
+    bottom: var(--positiony);
+    right: var(--positionx);
+    margin: 16px;
+    display: flex;
+    flex-direction: column;
+    z-index: 999
+  }
+
+  .df-btn-text {
+    min-width: 56px;
+    color: var(--textcolor);
+    display: inline-flex;
+    align-items: center;
+    font-weight: 500;
+    padding: 0 24px 0 0;
+    font-size: .875rem;
+    height: 48px
+  }
+
+  .df-btn-text:before {
+    min-width: 56px;
+    height: 48px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 24px;
+    background-image: var(--logo);
+    content: ''
+  }
+
+  .df-btn:hover {
+    box-shadow: 0 1px 3px 0 rgba(60,64,67,0.302), 0 4px 8px 3px rgba(60,64,67,0.149)
+  }
+
+  .df-btn:not(.df-closed) {
+    border-radius: calc(var(--borderradius) / 2)
+  }
+
+  .df-btn:not(.df-closed) > .df-btn-text:before {
+    background: var(--textcolor);
+    -webkit-mask: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>') no-repeat center;
+    mask: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>') no-repeat center
+  }
+
+  .df-btn-content {
+    display: flex;
+		align-self: flex-end;
+    height: var(--height);
+    width: var(--width);
+    transition: all .45s cubic-bezier(.4, 0, .2, 1);
+    opacity: 1
+  }
+
+  .df-btn:not(.df-closed) > .df-btn-content {
+    padding-bottom: 16px;
+  }
+
+	.df-btn-content iframe {
+		height: 100%;
+		width: 100%;
+		border: none;
+	}
+
+  .df-closed > .df-btn-content {
+    width: 0;
+    height: 0;
+    opacity: 0
+  }
+
+  @media screen and (max-width: 720px) {
+    .df-btn:not(.df-closed) {
+      margin: 0px;
+      border-radius: 0px
+    }
+
+    .df-btn:not(.df-closed) > .df-btn-content {
+      width: 100vw;
+      max-height: 100vh;
+      height: calc(100vh - 56px);
+      padding-bottom: 0px
+    }
+
+    .df-btn-text {
+      padding: 0;
+      height: 56px;
+      font-size: 0;
+    }
+  }
+</style>
